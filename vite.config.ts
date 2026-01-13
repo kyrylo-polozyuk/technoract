ba281b432dd8291c07f0a351edbb995c839be183
KepzAT <kyryl.polozyuk@audiotool.com>
2026-01-13 19:07:31 +0100

    add workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/",
   server: {
     host: "127.0.0.1",
     port: 5173,
